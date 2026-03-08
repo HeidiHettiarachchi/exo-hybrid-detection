665fe7b338f3a8e8af7641ee2c79b1d09b4f147b
HeidiHettiarachchi <it22323248@my.sliit.lk>
2026-03-08 16:13:01 +0530

    fixed localhost calling error

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,11 @@
+// import axios from "axios";
+
+// export const api = axios.create({
+//   baseURL: "http://localhost:8000"
+// });
+
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "https://exo-hybrid-detection.onrender.com"
 });
