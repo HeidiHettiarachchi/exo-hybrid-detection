c3a054b21f3ff05cc0125bdd15964a15d948e1e1
HeidiHettiarachchi <it22323248@my.sliit.lk>
2026-03-08 16:06:47 +0530

    new frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,36 +1,3 @@
-// // import { defineConfig } from "vite";
-// // import react from "@vitejs/plugin-react";
-
-// // export default defineConfig({
-// //   plugins: [react()],
-// //   server: {
-// //     port: 3000 
-// //   }
-// // });
-
-// import { defineConfig } from "vite";
-// import react from "@vitejs/plugin-react";
-
-// export default defineConfig({
-//   plugins: [react()],
-//   server: {
-//     port: 3000,
-//     proxy: {
-//       "/api": {
-//         target: "https://exo-hybrid-detection.onrender.com",
-//         changeOrigin: true,
-//       },
-//     },
-//   },
-//   define: {
-//     __API_BASE__: JSON.stringify(
-//       process.env.NODE_ENV === "production"
-//         ? "https://exo-hybrid-detection.onrender.com"
-//         : "http://localhost:8000"
-//     ),
-//   },
-// });
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -39,7 +6,7 @@ export default defineConfig({
   server: {
     port: 3000,
     host: true,
-    allowedHosts: ["exoplanet-hybrid-detection-frontend.onrender.com"],
+    allowedHosts: ["exoplanet-detection-sp06.onrender.com"],
     proxy: {
       "/api": {
         target: "https://exo-hybrid-detection.onrender.com",
@@ -50,13 +17,9 @@ export default defineConfig({
   preview: {
     port: 3000,
     host: true,
-    allowedHosts: ["exoplanet-hybrid-detection-frontend.onrender.com"],
+    allowedHosts: ["exoplanet-detection-sp06.onrender.com"],
   },
-  define: {
-    __API_BASE__: JSON.stringify(
-      process.env.NODE_ENV === "production"
-        ? "https://exo-hybrid-detection.onrender.com"
-        : "http://localhost:8000"
-    ),
-  },
+  ddefine: {
+  __API_BASE__: JSON.stringify("https://exo-hybrid-detection.onrender.com"),
+},
 });
