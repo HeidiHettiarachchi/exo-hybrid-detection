a3b01127e99a61d959c25ee8ed907a5835151675
HeidiHettiarachchi <it22323248@my.sliit.lk>
2026-03-09 11:27:13 +0530

    modified

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -8,8 +8,8 @@ import axios from "axios";
 
 export const api = axios.create({
  
-    baseURL: "http://localhost:8000"
-       
+    // baseURL: "http://localhost:8000"
+    baseURL: "https://exo-hybrid-detection-backend.onrender.com"   
 
 
 });
